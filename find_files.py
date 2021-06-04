0215b85138706ac462f8010de2ed38d6d910f8fc
David Wallace <dwallacemarketing@gmail.com>
2021-06-03 22:19:43 -0500

    rebuild

--- find_files.py
+++ find_files.py
@@ -4,7 +4,7 @@ import shutil
 import glob
 
 # find all files
-path = 'C:\\Users\\David\\Desktop\\example_downloads_folder\\start'
+path = os.getcwd()
 files = os.listdir(path)
 # todays date prefix
 date_prefix = date.today()
@@ -13,11 +13,12 @@ version = 0
 # build the path
 string = str(date_prefix) + '_V' + str(version) + '_' + 'EXAMPLE'
 
+# add logic to iterate version number here
+print(os.getcwd().isdir(string))
+
 # create a new folder -- add prefix -- confirm verison number
 os.mkdir(string)
 
-# add login to iterate version number here
-#   if
 
 # confirm director and show all files in the directory
 print('the CWD is: ' + os.getcwd())
