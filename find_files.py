1f291d782d6c5ecb4b58a82cae05be0919cc042d
David Wallace <dwallacemarketing@gmail.com>
2021-06-03 21:47:42 -0500

    baby iteration

--- find_files.py
+++ find_files.py
@@ -1,3 +1,4 @@
+from datetime import date
 import os
 import shutil
 import glob
@@ -5,30 +6,28 @@ import glob
 # find all files
 path = 'C:\\Users\\David\\Desktop\\example_downloads_folder\\start'
 files = os.listdir(path)
+# todays date prefix
+date_prefix = date.today()
+# version number for global variable
+version = 0
 
-for file in files:
-    print('Here is one of all files in this folder.... ' + file)
-
-# find only files that fit a criteria - .txt
-os.chdir(path)
-print('current directory is ' + path)
-
-for file in glob.glob('*.txt'):
-    print('txt file --> ' + file)
+print('todays date is ' + str(date_prefix))
 
 # confirm the CWD
 print('the CWD is: ' + os.getcwd())
 
-# move up the CWD
-os.chdir('/tmp')
+# create a new folder -- add prefix -- confirm verison number
+os.mkdir(str(date_prefix) + '_V' + str(version) + '_' + 'EXAMPLE2')
 
-# qa the CWD
-print('the CWD is: ' + os.getcwd())
-
-# copy files to new folder
+# add login to iterate version number
 
 
-# add pre-fix & label files with the date they were moved
+# confirm director and show all files in the directory
+print('current directory is ' + path)
+for file in files:
+    print('Here is one of all files in this folder.... ' + file)
 
 
-#
+# find only files that fit a criteria - .txt
+for file in glob.glob('*.txt'):
+    print('txt file --> ' + file)
